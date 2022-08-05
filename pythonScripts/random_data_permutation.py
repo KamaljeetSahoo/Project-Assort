535622ef39bcb955aad155a120e06ba839ca2549
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-08-06 01:11:39 +0530

    Create random_data_permutation.py

---
+++ pythonScripts/random_data_permutation.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from itertools import permutations
+
+def permutation(lst):
+  if len(lst) == 0:
+      return []
+
+  if len(lst) == 1:
+      return [lst]
+
+
+  l = [] 
+
+  # Iterate the input(lst) and calculate the permutation
+  for i in range(len(lst)):
+      m = lst[i]
+      remLst = lst[:i] + lst[i+1:]
+
+      for p in permutation(remLst):
+          l.append([m] + p)
+  return l
+ 
+#   As input 
+list1=['0.3','0.4','0.1','0.1','0.1']
+l2=(permutation(list1))
+mySet = set()
+
+for j in range(len(l2)):
+  mySet = mySet | set([tuple(l2[j])])
+
