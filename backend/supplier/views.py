7f5e6dcaaec4ae97fe7ab73fcc6f9da7b94dad9a
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-08-06 13:08:24 +0530

    Modular URL routing with home page added

--- backend/supplier/views.py
+++ backend/supplier/views.py
@@ -1,3 +1,11 @@
+import re
 from django.shortcuts import render
+from .models import Supplier, Region
 
 # Create your views here.
+def homepage(request):
+    context = {
+        'supplier': Supplier.objects.all(),
+        'region': Region.objects.all(),
+    }
+    return render(request, 'supplier/home.html', context=context)
