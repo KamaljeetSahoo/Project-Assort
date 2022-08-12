21832c9677526f2174d0679561dc184b4f818c90
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-08-12 16:03:39 +0530

    Added Service, Function and Country fields to the database

--- backend/supplier/admin.py
+++ backend/supplier/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Region, Supplier
+from .models import Region, Supplier, Country, Function, Service
 
 # Register your models here.
 admin.site.register(Region)
 admin.site.register(Supplier)
+admin.site.register(Country)
+admin.site.register(Function)
+admin.site.register(Service)
--- backend/supplier/models.py
+++ backend/supplier/models.py
@@ -6,10 +6,31 @@ class Region(models.Model):
     
     def __str__(self) -> str:
         return str(self.region_code)
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return str(self.country_name)
+
+class Function(models.Model):
+    company_func = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return str(self.company_func)
+    
+class Service(models.Model):
+    service_name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return str(self.service_name)
     
 class Supplier(models.Model):
     supplier_name = models.CharField(max_length=10)
     region = models.ForeignKey(Region, null=True, blank=True, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.CASCADE)
+    supplier_function = models.ForeignKey(Function, null=True, blank=True, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, null=True, blank=True, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return str(self.supplier_name)
