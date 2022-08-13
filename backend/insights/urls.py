964274cffafe97fb3fb40f64a434c332e70b56f6
adyashajena-26 <79363736+adyashajena-26@users.noreply.github.com>
2022-08-13 15:32:14 +0530

    charts

--- backend/insights/urls.py
+++ backend/insights/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import insights_page
 
 urlpatterns = [
+    path('insights/', insights_page,name='insights_page'),
     
 ]
--- backend/insights/views.py
+++ backend/insights/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def insights_page(request):
+    return render(request, 'insights/insights_page.html')
