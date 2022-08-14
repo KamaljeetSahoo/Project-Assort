31e630171d98d92503df62c11338ba7910d1c704
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-08-14 15:24:35 +0530

    Historical data visualisation added

--- backend/insights/urls.py
+++ backend/insights/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import insights_page
+from .views import insights_page, historical_insights, company_history_view, get_services
 
 urlpatterns = [
     path('insights/', insights_page,name='insights_page'),
-    
+    path('history/', historical_insights, name='history'),
+    path('companyHistory/<int:company_id>/', company_history_view, name='companyHistory'),
+    path('api/services/<int:company_id>/', get_services),
 ]
--- backend/insights/views.py
+++ backend/insights/views.py
@@ -1,5 +1,32 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from supplier.models import Company, Service
 
 # Create your views here.
 def insights_page(request):
     return render(request, 'insights/insights_page.html')
+
+def historical_insights(request):
+    context = {
+        'companies': Company.objects.all(),
+    }
+    return render(request, 'insights/history.html', context=context)
+
+def company_history_view(request, company_id):
+    company = Company.objects.get(id=company_id)
+    suppliers = company.supplier_set.all()
+    years = list(suppliers.values('year').distinct())
+    context = {
+        'company': company,
+        'suppliers': suppliers,
+        'years': years,
+    }
+    return render(request, 'insights/companyHistory.html', context=context)
+
+def get_services(request, company_id):
+    company = Company.objects.get(id=company_id)
+    service = list(company.supplier_set.all().values('service').distinct())
+    services = []
+    for ser in service:
+        services.append(Service.objects.get(id=ser['service']).service_name)
+    return JsonResponse(services, safe=False)
